17a9102794f9b78cef8a853e50c2c5b9675d06f1
drumon-iq <arifdbr@gmail.com>
2025-02-25 21:19:30 -0300

    Init commit

---
+++ src/test/kotlin/br/com/cooltech/CooltechApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.cooltech
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CooltechApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
